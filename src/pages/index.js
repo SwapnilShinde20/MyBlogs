7629a90563c8f4816bc3da8dfa620d4302121fce
SwapnilShinde20 <shindeswapnil2005@gmail.com>
2024-12-26 15:56:10 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import AddPost from './AddPost'
+import AllPosts from './AllPosts'
+import EditPost from './EditPost'
+import Home from './Home'
+import LogIn from './LogIn'
+import SignUp from './SignUp'
+import Post from './Post'
+
+
+
+export{
+    AddPost,
+    AllPosts,
+    EditPost,
+    Home,
+    LogIn,
+    Post,
+    SignUp
+}
